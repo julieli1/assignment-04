a242f89f272213994c6f45a0cf660aaad3da494b
julieli1 <missvckise@gmail.com>
2021-03-24 23:30:06 +0000

    delete useless part

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ document.getElementById('bibliography').addEventListener('click', bibliography)
 function bibliography () {
   document.getElementById('text').innerHTML = 'https://lisa-anthony.com/tag/natural-user-interaction/ https://en.wikipedia.org/wiki/Lisa_Anthony'
 }
-
-/*
-function bibliography () {
-  document.getElementById('text'). = "https://lisa-anthony.com/tag/natural-user-interaction/ https://en.wikipedia.org/wiki/Lisa_Anthony "
-}
-*/
