7acd3ec00f300dd4d94fffc7fadc173de376874a
julieli1 <missvckise@gmail.com>
2021-03-24 20:02:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('bibliography').addEventListener('click', bibliography)
+
+
+function bibliography () {
+  document.getElementById('text').innerHTML = "https://lisa-anthony.com/tag/natural-user-interaction/ https://en.wikipedia.org/wiki/Lisa_Anthony "
+}
+
+/*
+function bibliography () {
+  document.getElementById('text'). = "https://lisa-anthony.com/tag/natural-user-interaction/ https://en.wikipedia.org/wiki/Lisa_Anthony "
+}
+*/
