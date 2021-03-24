248d933a32f24c78ed92bbcc9158fa73cc1161c3
julieli1 <missvckise@gmail.com>
2021-03-24 22:50:51 +0000

    javascript syntax

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.getElementById('bibliography').addEventListener('click', bibliography)
 
-
 function bibliography () {
-  document.getElementById('text').innerHTML = "https://lisa-anthony.com/tag/natural-user-interaction/ https://en.wikipedia.org/wiki/Lisa_Anthony "
+  document.getElementById('text').innerHTML = 'https://lisa-anthony.com/tag/natural-user-interaction/ https://en.wikipedia.org/wiki/Lisa_Anthony'
 }
 
 /*
